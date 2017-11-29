a4b15212f26940c8141176904dfe933fbbca785e
Tanawat Tassana <amp.tanawat@gmail.com>
2017-11-29 21:19:56 +0700

    Use Chai as assertion library

--- test/hotoken_reservation.js
+++ test/hotoken_reservation.js
@@ -1,10 +1,11 @@
+const {expect} = require('chai')
 const HotokenReservation = artifacts.require('./HotokenReservation')
 
 contract('HotokenReservation', function(accounts) {
 
   it('should be able to deployed', async function() {
     const instance = await HotokenReservation.deployed()
-    assert.ok(instance)
+    expect(instance).to.be.ok
   })
 
 })
