20df346da533879dff720fa3e56ada15ccb052be
Parinthorn Panya <mai@proteus-tech.com>
2017-11-30 16:05:00 +0700

    Test for add new adress and limit to whitelist

--- contracts/HotokenReservation.sol
+++ contracts/HotokenReservation.sol
@@ -7,4 +7,12 @@ contract HotokenReservation {
     function HotokenReservation() public {
 
     }
+
+    function addToWhitelist(address _newAddress, uint _amount) public {
+        whitelist[_newAddress] = _amount;
+    }
+
+    function getLimitOf(address _address) public view returns(uint) {
+        return whitelist[_address];
+    }
 }
--- test/hotoken_reservation.js
+++ test/hotoken_reservation.js
@@ -2,7 +2,7 @@ const {expect} = require('chai')
 const HotokenReservation = artifacts.require('./HotokenReservation')
 
 contract('HotokenReservation', function(accounts) {
-
+  
   it('should be able to deployed', async function() {
     const instance = await HotokenReservation.deployed()
     expect(instance).to.be.ok
@@ -11,6 +11,15 @@ contract('HotokenReservation', function(accounts) {
   it('should create empty whitelist', async function() {
     const instance = await HotokenReservation.deployed()
     expect(instance.whitelist).to.be.ok
+  })
+
+  it('should be able to add new adress with limit to whitelist', async function() {
+    const newAccount = accounts[1]
+    const limit = 100
+    const instance = await HotokenReservation.deployed()
+    await instance.addToWhitelist(newAccount, limit)
+    const whitelistLimit = await instance.whitelist.call(newAccount)
+    expect(whitelistLimit.toNumber()).to.equal(limit)
   })
 
 })
