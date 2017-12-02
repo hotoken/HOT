89ef2849d98e615b924d38c6a7cca02bd867db50
Parinthorn Panya <mai@proteus-tech.com>
2017-12-02 23:32:27 +0700

    Test for events Tokenpurchase in case of buying successful

--- contracts/HotokenReservation.sol
+++ contracts/HotokenReservation.sol
@@ -14,13 +14,8 @@ contract HotokenReservation is StandardToken, Ownable {
     // Fixed rate for now
     uint256 public constant rate = 2 * (10 ** uint256(3));
 
+    uint256 public tokenSold;
     uint256 public weiRaised;
-
-    struct ReservedHotToken {
-        address owner;
-        uint token;
-        uint ethAmount;
-    }
 
     mapping(address=>uint) public whitelist;
 
@@ -36,6 +31,7 @@ contract HotokenReservation is StandardToken, Ownable {
     function HotokenReservation() public {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
+        tokenSold = 0;
         weiRaised = 0;
     }
 
@@ -48,13 +44,14 @@ contract HotokenReservation is StandardToken, Ownable {
         require(beneficiary != address(0));
         require(owner != beneficiary);
         require(whitelist[beneficiary] == 1);
-        require(weiRaised <= totalSupply);
+        require(tokenSold <= totalSupply);
 
         uint256 weiAmount = msg.value;
         // calculate token amount to be created
         uint256 tokens = weiAmount.mul(rate);
 
         // update state
+        tokenSold = tokenSold.add(tokens);
         weiRaised = weiRaised.add(weiAmount);
 
         // transfer token to purchaser
@@ -64,21 +61,21 @@ contract HotokenReservation is StandardToken, Ownable {
         TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
     }
 
-    function addToWhitelist(address _newAddress) public {
+    function addToWhitelist(address _newAddress) public onlyOwner {
         whitelist[_newAddress] = 1;
     }
 
-    function addManyToWhitelist(address[] _newAddresses) public {
+    function addManyToWhitelist(address[] _newAddresses) public onlyOwner {
         for (uint i = 0; i < _newAddresses.length; i++) {
             whitelist[_newAddresses[i]] = 1;
         }
     }
 
-    function removeFromWhiteList(address _address) public {
+    function removeFromWhiteList(address _address) public onlyOwner {
         whitelist[_address] = 0;
     }
 
-    function removeManyFromWhitelist(address[] _addresses) public {
+    function removeManyFromWhitelist(address[] _addresses) public onlyOwner {
         for (uint i = 0; i < _addresses.length; i++) {
             whitelist[_addresses[i]] = 0;
         }
--- test/hotoken_reservation.js
+++ test/hotoken_reservation.js
@@ -20,6 +20,7 @@ contract('HotokenReservation', function(accounts) {
     expect(result).to.be.ok
     expect(result).to.be.not.empty
     expect(Object.keys(result)).to.have.lengthOf(3)
+    expect(result.receipt.status).to.be.equal(1)
   })
 
   it('should be able check address in the whitelist', async function() {
@@ -36,6 +37,7 @@ contract('HotokenReservation', function(accounts) {
     expect(result).to.be.ok
     expect(result).to.be.not.empty
     expect(Object.keys(result)).to.have.lengthOf(3)
+    expect(result.receipt.status).to.be.equal(1)
     expect((await instance.whitelist.call(newAccounts[0])).toNumber()).to.be.equal(1)
     expect((await instance.whitelist.call(newAccounts[1])).toNumber()).to.be.equal(1)
     expect((await instance.whitelist.call(newAccounts[2])).toNumber()).to.be.equal(1)
@@ -73,18 +75,17 @@ contract('HotokenReservation buy token', function(accounts) {
     var amountWei = web3.toWei(amountEther, 'ether')
 
     const ownerEtherBefore = (await web3.eth.getBalance(accounts[0])).toNumber()
+    const tokenSoldBefore = (await instance.tokenSold.call()).toNumber()
 
-    try {
-      await instance.sendTransaction({from: user1, value: amountWei})
-      const user1BalanceAfter = (await instance.balanceOf(user1)).toNumber()
-      const ownerEtherAfter = (await web3.eth.getBalance(accounts[0])).toNumber()
+    await instance.sendTransaction({from: user1, value: amountWei})
+    const user1BalanceAfter = (await instance.balanceOf(user1)).toNumber()
+    const ownerEtherAfter = (await web3.eth.getBalance(accounts[0])).toNumber()
+    const tokenSoldAfter = (await instance.tokenSold.call()).toNumber()
 
-      expect(user1BalanceAfter).to.be.equal(rate * amountWei)
-      expect(ownerEtherAfter).to.be.above(ownerEtherBefore)
-      expect((await instance.weiRaised.call()).toNumber()).to.be.equal(Number(amountWei))
-    } catch (e) {
-      expect(e.toString()).to.be.include('revert')
-    }
+    expect(user1BalanceAfter).to.be.equal(rate * amountWei)
+    expect(ownerEtherAfter).to.be.above(ownerEtherBefore)
+    expect(tokenSoldAfter).to.be.equal(Number(tokenSoldBefore + (rate * amountWei)))
+    
   })
 
   it('should not be able to retrieve ether from address that it is not in the whitelist', async function() {
@@ -96,13 +97,14 @@ contract('HotokenReservation buy token', function(accounts) {
 
     var amountEther = 2
     var amountWei = web3.toWei(amountEther, 'ether')
+    const tokenSold = (await instance.tokenSold.call()).toNumber()
 
     try {
       await instance.sendTransaction({from: user2, value: amountWei})
     } catch (e) {
       expect(e.toString()).to.be.include('revert')
     }
-    expect((await instance.weiRaised.call()).toNumber()).to.be.equal(Number(amountWei))
+    expect(tokenSold).to.be.equal(Number(rate * amountWei))
   })
 
   it('should not be able to retrieve ether from owner contract address', async function() {
@@ -113,12 +115,43 @@ contract('HotokenReservation buy token', function(accounts) {
     var amountWei = web3.toWei(amountEther, 'ether')
 
     const ownerEtherBefore = (await web3.eth.getBalance(accounts[0])).toNumber()
+    const tokenSold = (await instance.tokenSold.call()).toNumber()
 
     try {
       await instance.sendTransaction({from: accounts[0], value: amountWei})
     } catch (e) {
       expect(e.toString()).to.be.include('revert')
     }
-    expect((await instance.weiRaised.call()).toNumber()).to.be.equal(Number(amountWei))
+    expect(tokenSold).to.be.equal(Number(rate * amountWei))
+  })
+
+  it('should be able to log event if send ether success', async function() {
+    const instance = await HotokenReservation.deployed()
+    const user2 = accounts[2]
+    const rate = (await instance.rate.call()).toNumber()
+    await instance.addToWhitelist(user2)
+
+    var amountEther = 5
+    var amountWei = web3.toWei(amountEther, 'ether')
+
+    const ownerEtherBefore = (await web3.eth.getBalance(accounts[0])).toNumber()
+    const tokenSoldBefore = (await instance.tokenSold.call()).toNumber()
+
+    const tx = await instance.sendTransaction({from: user2, value: amountWei})
+    const user2BalanceAfter = (await instance.balanceOf(user2)).toNumber()
+    const ownerEtherAfter = (await web3.eth.getBalance(accounts[0])).toNumber()
+    const tokenSoldAfter = (await instance.tokenSold.call()).toNumber()
+
+    expect(user2BalanceAfter).to.be.equal(rate * amountWei)
+    expect(ownerEtherAfter).to.be.above(ownerEtherBefore)
+    expect(tokenSoldAfter).to.be.equal(Number(tokenSoldBefore + (rate * amountWei)))
+
+    // check events log
+    expect(tx.logs).to.be.ok
+    expect(tx.logs[0].event).to.be.equal('TokenPurchase')
+    expect(tx.logs[0].args.purchaser).to.be.equal(user2)
+    expect(tx.logs[0].args.beneficiary).to.be.equal(user2)
+    expect(tx.logs[0].args.value.toNumber()).to.be.equal(Number(amountWei))
+    expect(tx.logs[0].args.amount.toNumber()).to.be.equal(rate * amountWei)
   })
 })
