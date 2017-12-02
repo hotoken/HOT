4b1b7f6c77f054589175acbde42c1314478dd424
Parinthorn Panya <mai@proteus-tech.com>
2017-12-02 17:09:53 +0700

    Remove limit, and make this contract as a Standard token and Ownable

--- contracts/HotokenReservation.sol
+++ contracts/HotokenReservation.sol
@@ -1,20 +1,15 @@
 pragma solidity ^0.4.17;
 
 import '../node_modules/zeppelin-solidity/contracts/token/StandardToken.sol';
+import '../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract HotokenReservation is StandardToken {
-
-    string public constant name = "Hotoken";
-    string public constant symbol = "HTKN";
+contract HotokenReservation is StandardToken, Ownable {
+    
+    string public constant name = "ReservedHotoken";
+    string public constant symbol = "RHTKN";
     uint8 public constant decimals = 18;
 
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
-
-    struct Reservation {
-        address owner;
-        uint limit;
-        uint reserved;
-    }
+    uint256 public constant INITIAL_SUPPLY = 3000000000 * (10 ** uint256(decimals));
 
     struct ReservedHotToken {
         address owner;
@@ -22,33 +17,27 @@ contract HotokenReservation is StandardToken {
         uint ethAmount;
     }
 
-    mapping(address=>Reservation) public whitelist;
-    mapping(uint=>mapping(address=>ReservedHotToken)) public reservedToken;
+    mapping(address=>uint) public whitelist;
+    // mapping(string=>mapping(address=>ReservedHotToken)) public reservedToken;
     
     function HotokenReservation() public {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
 
-    function () payable {
-        require(whitelist[msg.sender].reserved == 1);
-        require(whitelist[msg.sender].limit >= msg.value);
-        uint amount = msg.value;
-        reservedToken[30][msg.sender] = ReservedHotToken(msg.sender, 1000, amount);
+    // function () payable {
+    //     require(whitelist[msg.sender] == 1);
+    //     uint amount = msg.value;
+    //     reservedToken["30%"][msg.sender] = ReservedHotToken(msg.sender, 1000, amount);
+    // }
+
+    function addToWhitelist(address _newAddress) public {
+        whitelist[_newAddress] = 1;
     }
 
-    function addToWhitelist(address _newAddress, uint _amount) public {
-        whitelist[_newAddress] = Reservation(_newAddress, _amount, 1);
-    }
-
-    function addManyToWhitelist(address[] _newAddresses, uint[] _amounts) public {
+    function addManyToWhitelist(address[] _newAddresses) public {
         for (uint i = 0; i < _newAddresses.length; i++) {
-            whitelist[_newAddresses[i]] = Reservation(_newAddresses[i], _amounts[i], 1);
+            whitelist[_newAddresses[i]] = 1;
         }
-    }
-
-    function getLimitOf(address _address) public view returns(uint) {
-        require(whitelist[_address].reserved == 1);
-        return whitelist[_address].limit;
     }
 }
--- test/hotoken_reservation.js
+++ test/hotoken_reservation.js
@@ -13,56 +13,31 @@ contract('HotokenReservation', function(accounts) {
     expect(instance.whitelist).to.be.ok
   })
 
-  it('should be able to add new adress with limit to whitelist', async function() {
+  it('should be able to add new adress to whitelist', async function() {
     const newAccount = accounts[1]
-    const limit = 100
     const instance = await HotokenReservation.deployed()
-    const result = await instance.addToWhitelist(newAccount, limit)
+    const result = await instance.addToWhitelist(newAccount)
     expect(result).to.be.ok
     expect(result).to.be.not.empty
     expect(Object.keys(result)).to.have.lengthOf(3)
   })
 
-  it('should be able to retrive limit by address', async function() {
+  it('should be able check address in the whitelist', async function() {
     const account = accounts[1]
-    const limit = 100
     const instance = await HotokenReservation.deployed()
-    const whitelistLimit = await instance.getLimitOf.call(account)
-    expect(whitelistLimit.toNumber()).to.equal(limit)
-  })
-
-  it('should not be able to retrieve limit by address is not in whitelist', async function() {
-    const account = accounts[2]
-    const instance = await HotokenReservation.deployed()
-    try {
-      const whitelistLimit = await instance.getLimitOf.call(account)
-    } catch(e) {
-      expect(e.toString().indexOf('revert')).to.be.above(-1)
-    }
+    const exists = await instance.whitelist.call(account)
+    expect(exists.toNumber()).to.equal(1)
   })
 
   it('should be able to add many new addresses to the whitelist', async function() {
     const newAccounts = [accounts[2], accounts[3], accounts[4]]
-    const limits = [100, 200, 300]
     const instance = await HotokenReservation.deployed()
-    const result = await instance.addManyToWhitelist(newAccounts, limits)
+    const result = await instance.addManyToWhitelist(newAccounts)
     expect(result).to.be.ok
     expect(result).to.be.not.empty
     expect(Object.keys(result)).to.have.lengthOf(3)
-    expect((await instance.getLimitOf.call(newAccounts[0])).toNumber()).to.be.equal(limits[0])
-    expect((await instance.getLimitOf.call(newAccounts[1])).toNumber()).to.be.equal(limits[1])
-    expect((await instance.getLimitOf.call(newAccounts[2])).toNumber()).to.be.equal(limits[2])
+    expect((await instance.whitelist.call(newAccounts[0])).toNumber()).to.be.equal(1)
+    expect((await instance.whitelist.call(newAccounts[1])).toNumber()).to.be.equal(1)
+    expect((await instance.whitelist.call(newAccounts[2])).toNumber()).to.be.equal(1)
   })
-
-  it('should be able to update the limit of address in whitelist', async function() {
-    const account = accounts[1]
-    const limit = 1000
-    const instance = await HotokenReservation.deployed()
-    const result = await instance.addToWhitelist(account, limit)
-    expect(result).to.be.ok
-    expect(result).to.be.not.empty
-    expect(Object.keys(result)).to.have.lengthOf(3)
-    expect((await instance.getLimitOf.call(account)).toNumber()).to.be.equal(limit)
-  })
-
 })
