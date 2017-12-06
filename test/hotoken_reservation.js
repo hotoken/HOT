00081a33bd8ec80c081244676cee28a66b4f3ab3
Parinthorn Panya <mai@proteus-tech.com>
2017-12-06 10:16:53 +0700

    Test for kill the contract

--- test/hotoken_reservation.js
+++ test/hotoken_reservation.js
@@ -786,3 +786,37 @@ contract('HotokenReservation claim tokens', function(accounts) {
     expect(exists).to.be.false
   })
 })
+
+contract('HotokenReservation, kill contract by owner account', function(accounts) {
+
+  it('should be able to kill contract', async function() {
+    const instance = await HotokenReservation.deployed()
+    await instance.kill()
+
+    expect((await instance.owner.call())).to.be.equal("0x0")
+  })
+
+  it('should not be able to kill contract again if killed already', async function() {
+    const instance = await HotokenReservation.deployed()
+
+    try {
+      await instance.kill()
+    } catch (e) {
+      expect(e.toString()).to.be.include('revert')
+    }
+  })
+})
+
+contract('HotokenReservation, kill contract by not owner account', function(accounts) {
+  
+  it('should not be able to kill contract', async function() {
+    const instance = await HotokenReservation.deployed()
+    const user1 = accounts[1]
+
+    try {
+      await instance.kill({from: user1})
+    } catch (e) {
+      expect(e.toString()).to.be.include('revert')
+    }
+  })
+})
