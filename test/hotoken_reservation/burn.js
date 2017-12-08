a7f18b330602caf19ea9d55e0e42a15e80936e71
Tanawat Tassana <amp.tanawat@gmail.com>
2017-12-08 09:12:45 +0700

    Check totalSupply should equal to token sold after burn

--- test/hotoken_reservation/burn.js
+++ test/hotoken_reservation/burn.js
@@ -1,6 +1,34 @@
 const {expect} = require('chai')
 const HotokenReservation = artifacts.require('./HotokenReservation')
 
+contract('HotokenReservation', function(accounts) {
+  describe.only('burn', function() {
+    it('total supply should equal to token sold', async function() {
+      const h = await HotokenReservation.deployed()
+      const user1 = accounts[1]
+
+      await h.setPause(false)
+      await h.addToWhitelist(user1)
+      await h.setConversionRate('ETH', 50000)
+      await h.setDiscountRate(0) // no discount
+      await h.sendTransaction({from: user1, value: 10 * 10 ** 18})
+      await h.setSaleFinished(true)
+
+      const totalSupplyBefore = await h.totalSupply.call()
+      expect(totalSupplyBefore.toNumber()).to.be.equal(3000000000 * 10 ** 18)
+
+      const tokenSold = await h.getTokenSold()
+      expect(tokenSold.toNumber()).to.be.equal(50000 * 10 ** 18)
+
+      const tx = await h.burn()
+
+      const totalSupplyAfter = await h.totalSupply.call()
+      expect(totalSupplyAfter.toNumber()).to.be.equal(50000 * 10 ** 18)
+    })
+  })
+})
+
+/*
 contract('HotokenReservation, burn tokens', function(accounts) {
   let hotoken
 
@@ -62,3 +90,4 @@ contract('HotokenReservation, burn tokens', function(accounts) {
     }
   })
 })
+*/
